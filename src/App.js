7e4d6bba66fac4cc73e92f11be36233c8ae01141
jefrin james <jamesjefrin13@gmail.com>
2024-06-18 08:30:10 +0530

    added all files

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import LoadMoreData from './load-more-data';
+
+function App() {
+  return (
+    <div className="App">
+     
+      <LoadMoreData/>
+    </div>
+  );
+}
+
+export default App;
